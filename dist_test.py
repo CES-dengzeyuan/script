3207aca266485a82431712755208a68057bac5c6
CES-dengzeyuan <46039524+CES-dengzeyuan@users.noreply.github.com>
2022-08-05 16:40:42 +0800

    Update dist_test.py

--- dist_test.py
+++ dist_test.py
@@ -21,11 +21,11 @@ if __name__ == '__main__':
                 ips = ip0 + ":" + str(port) + ";" + ip1 + ":" + str(port) + ";" + ip2 + ":" + str(port)
                 if db == 'Aria':
                     command = "./bench_tpcc --log_dir=" + logdir + " --id=" + str(
-                        mid) + " --servers='" + ips + "' --protocol=Aria --partition_num=324 --threads=5 --batch_size=" \
+                        mid) + " --servers='" + ips + "' --protocol=Aria --partition_num=108 --threads=5 --batch_size=" \
                               + str(batch_size) + " --query=" + wkld
                 else:
                     command = "./bench_tpcc --log_dir=" + logdir + " --id=" + str(
-                        mid) + " --servers='" + ips + "' --protocol=Calvin --partition_num=324 --threads=5 --batch_size=" + str(
+                        mid) + " --servers='" + ips + "' --protocol=Calvin --partition_num=108 --threads=5 --batch_size=" + str(
                         batch_size) + " --query=" + wkld + " --replica_group=3  --same_batch=True"
                 print(command)
                 os.system(command)
@@ -38,11 +38,11 @@ if __name__ == '__main__':
                 ips = ip0 + ":" + str(port) + ";" + ip1 + ":" + str(port) + ";" + ip2 + ":" + str(port)
                 if db == 'Aria':
                     command = "./bench_tpcc --log_dir=" + logdir + " --id=" + str(
-                        mid) + " --servers='" + ips + "' --protocol=Aria --partition_num=324 --threads=5 --batch_size=" \
+                        mid) + " --servers='" + ips + "' --protocol=Aria --partition_num=108 --threads=5 --batch_size=" \
                               + str(batch_size) + " --query=neworder --neworder_dist=" + str(cr)
                 else:
                     command = "./bench_tpcc --log_dir=" + logdir + " --id=" + str(
-                        mid) + " --servers='" + ips + "' --protocol=Calvin --partition_num=324 --threads=5 --batch_size=" + str(
+                        mid) + " --servers='" + ips + "' --protocol=Calvin --partition_num=108 --threads=5 --batch_size=" + str(
                         batch_size) + " --query=neworder --replica_group=3  --same_batch=True --neworder_dist=" + str(
                         cr)
                 print(command)
@@ -56,11 +56,11 @@ if __name__ == '__main__':
                 ips = ip0 + ":" + str(port) + ";" + ip1 + ":" + str(port) + ";" + ip2 + ":" + str(port)
                 if db == 'Aria':
                     command = "./bench_tpcc --log_dir=" + logdir + " --id=" + str(
-                        mid) + " --servers='" + ips + "' --protocol=Aria --partition_num=324 --threads=5 --batch_size=" \
+                        mid) + " --servers='" + ips + "' --protocol=Aria --partition_num=108 --threads=5 --batch_size=" \
                               + str(batch_size) + " --query=neworder --payment_dist=" + str(cr)
                 else:
                     command = "./bench_tpcc --log_dir=" + logdir + " --id=" + str(
-                        mid) + " --servers='" + ips + "' --protocol=Calvin --partition_num=324 --threads=5 --batch_size=" + str(
+                        mid) + " --servers='" + ips + "' --protocol=Calvin --partition_num=108 --threads=5 --batch_size=" + str(
                         batch_size) + " --query=neworder --replica_group=3  --same_batch=True --payment_dist=" + str(
                         cr)
                 print(command)
@@ -74,11 +74,11 @@ if __name__ == '__main__':
                 ips = ip0 + ":" + str(port) + ";" + ip1 + ":" + str(port) + ";" + ip2 + ":" + str(port)
                 if db == 'Aria':
                     command = "./bench_tpcc --log_dir=" + logdir + " --id=" + str(
-                        mid) + " --servers='" + ips + "' --protocol=Aria --partition_num=324 --threads=" + str(
+                        mid) + " --servers='" + ips + "' --protocol=Aria --partition_num=108 --threads=" + str(
                         thread) + " --batch_size=" + str(batch_size) + " --query=neworder"
                 else:
                     command = "./bench_tpcc --log_dir=" + logdir + " --id=" + str(
-                        mid) + " --servers='" + ips + "' --protocol=Calvin --partition_num=324 --threads=" + str(
+                        mid) + " --servers='" + ips + "' --protocol=Calvin --partition_num=108 --threads=" + str(
                         thread) + " --batch_size=" + str(
                         batch_size) + " --query=neworder --replica_group=3  --same_batch=True"
                 print(command)
