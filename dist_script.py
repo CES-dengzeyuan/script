9037a392b19a45391b683c901ea9cef2459cf43e
CES-dengzeyuan <46039524+CES-dengzeyuan@users.noreply.github.com>
2022-08-06 22:34:52 +0800

    Update dist_script.py

--- dist_script.py
+++ dist_script.py
@@ -9,15 +9,15 @@ if __name__ == '__main__':
     ip0 = '172.22.32.22'
     ip1 = '172.22.32.23'
     ip2 = '172.22.32.24'
-    os.system('mkdir log')
-    os.system('mkdir log/Aria')
-    os.system('mkdir log/Calvin')
+#     os.system('mkdir log')
+#     os.system('mkdir log/Aria')
+#     os.system('mkdir log/Calvin')
     for db in dblist:
         # impact of workload
         for wkld in wkldlist:
             for batch_size in range(20, 520, 20):
                 logdir = 'log/' + db + '/' + wkld
-                os.system('mkdir ' + logdir)
+#                 os.system('mkdir ' + logdir)
                 ips = ip0 + ":" + str(port) + ";" + ip1 + ":" + str(port) + ";" + ip2 + ":" + str(port)
                 if db == 'Aria':
                     command = "./bench_tpcc --log_dir=" + logdir + " --id=" + str(
@@ -28,13 +28,13 @@ if __name__ == '__main__':
                         mid) + " --servers='" + ips + "' --protocol=Calvin --partition_num=324 --threads=5 --batch_size=" + str(
                         batch_size) + " --query=" + wkld + " --replica_group=3  --same_batch=True"
                 print(command)
-                os.system(command)
+#                 os.system(command)
                 port += 1
         # impact of neworder distribution
         for cr in cross_ratios:
             for batch_size in range(20, 520, 20):
                 logdir = 'log/' + db + '/new-' + str(cr)
-                os.system('mkdir ' + logdir)
+#                 os.system('mkdir ' + logdir)
                 ips = ip0 + ":" + str(port) + ";" + ip1 + ":" + str(port) + ";" + ip2 + ":" + str(port)
                 if db == 'Aria':
                     command = "./bench_tpcc --log_dir=" + logdir + " --id=" + str(
@@ -46,13 +46,13 @@ if __name__ == '__main__':
                         batch_size) + " --query=neworder --replica_group=3  --same_batch=True --neworder_dist=" + str(
                         cr)
                 print(command)
-                os.system(command)
+#                 os.system(command)
                 port += 1
         # impact of payment distribution
         for cr in cross_ratios:
             for batch_size in range(20, 520, 20):
                 logdir = 'log/' + db + '/pay-' + str(cr)
-                os.system('mkdir ' + logdir)
+#                 os.system('mkdir ' + logdir)
                 ips = ip0 + ":" + str(port) + ";" + ip1 + ":" + str(port) + ";" + ip2 + ":" + str(port)
                 if db == 'Aria':
                     command = "./bench_tpcc --log_dir=" + logdir + " --id=" + str(
@@ -64,13 +64,13 @@ if __name__ == '__main__':
                         batch_size) + " --query=neworder --replica_group=3  --same_batch=True --payment_dist=" + str(
                         cr)
                 print(command)
-                os.system(command)
+#                 os.system(command)
                 port += 1
         # impact of threads
         for thread in range(2, 5):
             for batch_size in range(20, 520, 20):
                 logdir = 'log/' + db + '/threads-' + str(thread)
-                os.system('mkdir ' + logdir)
+#                 os.system('mkdir ' + logdir)
                 ips = ip0 + ":" + str(port) + ";" + ip1 + ":" + str(port) + ";" + ip2 + ":" + str(port)
                 if db == 'Aria':
                     command = "./bench_tpcc --log_dir=" + logdir + " --id=" + str(
@@ -82,5 +82,5 @@ if __name__ == '__main__':
                         thread) + " --batch_size=" + str(
                         batch_size) + " --query=neworder --replica_group=3  --same_batch=True"
                 print(command)
-                os.system(command)
+#                 os.system(command)
                 port += 1
