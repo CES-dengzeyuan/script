d9dfec0daa16a69635c3a474e558094314b5ab77
CES-dengzeyuan <46039524+CES-dengzeyuan@users.noreply.github.com>
2022-08-06 23:07:12 +0800

    Update dist_script.py

--- dist_script.py
+++ dist_script.py
@@ -20,13 +20,9 @@ if __name__ == '__main__':
 #                 os.system('mkdir ' + logdir)
                 ips = ip0 + ":" + str(port) + ";" + ip1 + ":" + str(port) + ";" + ip2 + ":" + str(port)
                 if db == 'Aria':
-                    command = "./bench_tpcc --log_dir=" + logdir + " --id=" + str(
-                        mid) + " --servers='" + ips + "' --protocol=Aria --partition_num=324 --threads=5 --batch_size=" \
-                              + str(batch_size) + " --query=" + wkld
+                    command = "./bench_tpcc --log_dir=" + logdir + " --id=" + str(mid) + " --servers='" + ips + "' --protocol=Aria --partition_num=324 --threads=5 --batch_size=" + str(batch_size) + " --query=" + wkld
                 else:
-                    command = "./bench_tpcc --log_dir=" + logdir + " --id=" + str(
-                        mid) + " --servers='" + ips + "' --protocol=Calvin --partition_num=324 --threads=5 --batch_size=" + str(
-                        batch_size) + " --query=" + wkld + " --replica_group=3  --same_batch=True"
+                    command = "./bench_tpcc --log_dir=" + logdir + " --id=" + str(mid) + " --servers='" + ips + "' --protocol=Calvin --partition_num=324 --threads=5 --batch_size=" + str(batch_size) + " --query=" + wkld + " --replica_group=3  --same_batch=True"
                 print(command)
 #                 os.system(command)
                 port += 1
@@ -37,14 +33,9 @@ if __name__ == '__main__':
 #                 os.system('mkdir ' + logdir)
                 ips = ip0 + ":" + str(port) + ";" + ip1 + ":" + str(port) + ";" + ip2 + ":" + str(port)
                 if db == 'Aria':
-                    command = "./bench_tpcc --log_dir=" + logdir + " --id=" + str(
-                        mid) + " --servers='" + ips + "' --protocol=Aria --partition_num=324 --threads=5 --batch_size=" \
-                              + str(batch_size) + " --query=neworder --neworder_dist=" + str(cr)
+                    command = "./bench_tpcc --log_dir=" + logdir + " --id=" + str(mid) + " --servers='" + ips + "' --protocol=Aria --partition_num=324 --threads=5 --batch_size=" + str(batch_size) + " --query=neworder --neworder_dist=" + str(cr)
                 else:
-                    command = "./bench_tpcc --log_dir=" + logdir + " --id=" + str(
-                        mid) + " --servers='" + ips + "' --protocol=Calvin --partition_num=324 --threads=5 --batch_size=" + str(
-                        batch_size) + " --query=neworder --replica_group=3  --same_batch=True --neworder_dist=" + str(
-                        cr)
+                    command = "./bench_tpcc --log_dir=" + logdir + " --id=" + str(mid) + " --servers='" + ips + "' --protocol=Calvin --partition_num=324 --threads=5 --batch_size=" + str(batch_size) + " --query=neworder --replica_group=3  --same_batch=True --neworder_dist=" + str(cr)
                 print(command)
 #                 os.system(command)
                 port += 1
@@ -55,14 +46,9 @@ if __name__ == '__main__':
 #                 os.system('mkdir ' + logdir)
                 ips = ip0 + ":" + str(port) + ";" + ip1 + ":" + str(port) + ";" + ip2 + ":" + str(port)
                 if db == 'Aria':
-                    command = "./bench_tpcc --log_dir=" + logdir + " --id=" + str(
-                        mid) + " --servers='" + ips + "' --protocol=Aria --partition_num=324 --threads=5 --batch_size=" \
-                              + str(batch_size) + " --query=neworder --payment_dist=" + str(cr)
+                    command = "./bench_tpcc --log_dir=" + logdir + " --id=" + str(mid) + " --servers='" + ips + "' --protocol=Aria --partition_num=324 --threads=5 --batch_size=" + str(batch_size) + " --query=neworder --payment_dist=" + str(cr)
                 else:
-                    command = "./bench_tpcc --log_dir=" + logdir + " --id=" + str(
-                        mid) + " --servers='" + ips + "' --protocol=Calvin --partition_num=324 --threads=5 --batch_size=" + str(
-                        batch_size) + " --query=neworder --replica_group=3  --same_batch=True --payment_dist=" + str(
-                        cr)
+                    command = "./bench_tpcc --log_dir=" + logdir + " --id=" + str(mid) + " --servers='" + ips + "' --protocol=Calvin --partition_num=324 --threads=5 --batch_size=" + str(batch_size) + " --query=neworder --replica_group=3  --same_batch=True --payment_dist=" + str(cr)
                 print(command)
 #                 os.system(command)
                 port += 1
@@ -73,14 +59,9 @@ if __name__ == '__main__':
 #                 os.system('mkdir ' + logdir)
                 ips = ip0 + ":" + str(port) + ";" + ip1 + ":" + str(port) + ";" + ip2 + ":" + str(port)
                 if db == 'Aria':
-                    command = "./bench_tpcc --log_dir=" + logdir + " --id=" + str(
-                        mid) + " --servers='" + ips + "' --protocol=Aria --partition_num=324 --threads=" + str(
-                        thread) + " --batch_size=" + str(batch_size) + " --query=neworder"
+                    command = "./bench_tpcc --log_dir=" + logdir + " --id=" + str(mid) + " --servers='" + ips + "' --protocol=Aria --partition_num=324 --threads=" + str(thread) + " --batch_size=" + str(batch_size) + " --query=neworder"
                 else:
-                    command = "./bench_tpcc --log_dir=" + logdir + " --id=" + str(
-                        mid) + " --servers='" + ips + "' --protocol=Calvin --partition_num=324 --threads=" + str(
-                        thread) + " --batch_size=" + str(
-                        batch_size) + " --query=neworder --replica_group=3  --same_batch=True"
+                    command = "./bench_tpcc --log_dir=" + logdir + " --id=" + str(mid) + " --servers='" + ips + "' --protocol=Calvin --partition_num=324 --threads=" + str(thread) + " --batch_size=" + str(batch_size) + " --query=neworder --replica_group=3  --same_batch=True"
                 print(command)
 #                 os.system(command)
                 port += 1
