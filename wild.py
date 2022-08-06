54f42f0f51824fae14a72ced2773cab16fa16daf
CES-dengzeyuan <46039524+CES-dengzeyuan@users.noreply.github.com>
2022-08-06 23:10:25 +0800

    Create wild.py

---
+++ wild.py
@@ -0,0 +1,27 @@
+import os
+
+if __name__ == '__main__':
+    dblist = ['Aria', 'Calvin']
+    wkldlist = ['neworder', 'payment', 'mixed']
+    mid = 0
+    port = 12345
+    ip0 = '172.22.32.22'
+    ip1 = '172.22.32.23'
+    ip2 = '172.22.32.24'
+    os.system('mkdir log')
+    os.system('mkdir log/Aria')
+    os.system('mkdir log/Calvin')
+    for db in dblist:
+        # impact of workload
+        for wkld in wkldlist:
+            for batch_size in range(20, 520, 20):
+                logdir = 'log/' + db + '/' + wkld
+                os.system('mkdir ' + logdir)
+                ips = ip0 + ":" + str(port) + ";" + ip1 + ":" + str(port) + ";" + ip2 + ":" + str(port)
+                if db == 'Aria':
+                    command = "./bench_tpcc --log_dir=" + logdir + " --id=" + str(mid) + " --servers='" + ips + "' --protocol=Aria --partition_num=324 --threads=5 --batch_size=" + str(batch_size) + " --query=" + wkld
+                else:
+                    command = "./bench_tpcc --log_dir=" + logdir + " --id=" + str(mid) + " --servers='" + ips + "' --protocol=Calvin --partition_num=324 --threads=5 --batch_size=" + str(batch_size) + " --query=" + wkld + " --replica_group=3  --same_batch=True"
+                print(command)
+                os.system(command)
+                port += 1
