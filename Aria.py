1d55b320df179984d02eeffeb5fb2bd4e0f584a6
CES-dengzeyuan <46039524+CES-dengzeyuan@users.noreply.github.com>
2022-08-21 22:49:50 +0800

    Update Aria.py

--- Aria.py
+++ Aria.py
@@ -1,15 +1,24 @@
+import os
 
-
- # impact of payment distribution
+if __name__ == '__main__':
+    dblist = ['Aria', 'Calvin']
+    wkldlist = ['neworder', 'payment', 'mixed']
+    cross_ratios = [0, 20, 80, 100]
+    mid = 0
+    port = 12345
+    ip0 = '172.22.32.37'
+    ip1 = '172.22.32.38'
+    ip2 = '172.22.32.39'
+    os.system('mkdir log')
+    os.system('mkdir log/Aria')
+    for db in dblist:
+        # impact of payment distribution
         for cr in cross_ratios:
             for batch_size in range(20, 520, 20):
                 logdir = 'log/' + db + '/pay-' + str(cr)
                 os.system('mkdir ' + logdir)
                 ips = ip0 + ":" + str(port) + ";" + ip1 + ":" + str(port) + ";" + ip2 + ":" + str(port)
-                if db == 'Aria':
-                    command = "./bench_tpcc --log_dir=" + logdir + " --id=" + str(mid) + " --servers='" + ips + "' --protocol=Aria --partition_num=324 --threads=5 --batch_size=" + str(batch_size) + " --query=payment --payment_dist=" + str(cr)
-                else:
-                    command = "./bench_tpcc --log_dir=" + logdir + " --id=" + str(mid) + " --servers='" + ips + "' --protocol=Calvin --partition_num=324 --threads=5 --batch_size=" + str(batch_size) + " --query=payment --replica_group=3  --same_batch=True --payment_dist=" + str(cr)
+                command = "./bench_tpcc --log_dir=" + logdir + " --id=" + str(mid) + " --servers='" + ips + "' --protocol=Aria --partition_num=324 --threads=5 --batch_size=" + str(batch_size) + " --query=payment --payment_dist=" + str(cr)
                 print(command)
                 os.system(command)
                 port += 1
